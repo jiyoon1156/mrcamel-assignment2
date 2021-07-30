bb66a32c38852ee9fb09db8a3f2c774b3d8ca3c4
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-07-30 16:27:14 +0900

    [FEAT] 상품 조회이력 목록 페이지 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Main from 'pages/Main';
+// import Main from 'pages/Main';
+import RecentList from 'pages/RecentList/RecentList';
 
 function App() {
   return (
     <div>
-      <Main />
+      <RecentList />
     </div>
   );
 }
---
+++ src/pages/RecentList/ProductList/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class ProductList extends Component {
+  render() {
+    return <ListContainer>ProductList</ListContainer>;
+  }
+}
+
+const ListContainer = styled.div``;
+
+export default ProductList;
