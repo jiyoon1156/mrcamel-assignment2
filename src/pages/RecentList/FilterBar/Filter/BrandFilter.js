e166963dcbf6f818ed4cf1db22bd3970bb974c64
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-07-31 00:17:10 +0900

    [FEAT] select 버튼 구현

--- src/pages/RecentList/FilterBar/Filter/BrandFilter.js
+++ src/pages/RecentList/FilterBar/Filter/BrandFilter.js
@@ -1,10 +1,90 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-/* eslint-disable */
 class BrandFilter extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     date: false,
+  //   };
+  // }
+
+  // handleDrop = () => {
+  //   this.setState({
+  //     date: true,
+  //   });
+  //   console.log(date);
+  // };
+  constructor() {
+    super();
+    this.state = {
+      isDroped: '',
+    };
+  }
+
+  handleDrop = () => {
+    this.setState({ isDroped: !this.state.isDroped });
+  };
+
   render() {
-    return <div>BrandFilter</div>;
+    const { isDroped } = this.state;
+    return (
+      <BrandBox>
+        <OptionBox onClick={this.handleDrop}>
+          <span>Brand</span> <i className="fas fa-sort-down" />
+        </OptionBox>
+        <BrandSelect isDrop={isDroped}>
+          <DropList>Nike</DropList>
+          <DropList>Nike</DropList>
+          <DropList>Nike</DropList>
+          <DropList>Nike</DropList>
+          <DropList>Nike</DropList>
+        </BrandSelect>
+      </BrandBox>
+    );
   }
 }
+
+const BrandBox = styled.div``;
+
+const OptionBox = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 40px;
+  border-radius: 6px;
+  box-shadow: 0 5px 10px 0 rgb(0 0 0 / 10%);
+  background-color: #fff;
+  color: #333333;
+
+  span {
+    margin-right: 10px;
+  }
+
+  i {
+    padding-bottom: 7px;
+  }
+
+  &:hover {
+    opacity: 0.7;
+    color: gray;
+  }
+`;
+
+const BrandSelect = styled.div`
+  position: absolute;
+`;
+
+const DropList = styled.div`
+  display: ${(props) => (props.isDrop ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 40px;
+  border: 1px solid #ededed;
+  border-radius: 6px;
+  background-color: #fff;
+`;
 
 export default BrandFilter;
