4d8bf7de4e45379525381a4190f96cf56b926ea1
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-07-31 13:51:29 +0900

    [FEAT] Product 페이지 마무리
    - 관심 없음 클릭
    - 랜덤 상품 조회 클릭

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -6,14 +6,24 @@ import Storage from 'utils/Storage';
 
 const DATA_JSON_API = 'data/data.json';
 const PLACEHOLDER_POST_API = 'https://jsonplaceholder.typicode.com/posts/';
+const NO_INTEREST_STORAGE_KEY = 'noInterest';
+const RECENT_LIST_STORAGE_KEY = 'recentList';
 
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { products: [], dummyDescription: '' };
+    this.state = {
+      products: [],
+      dummyDescription: '',
+      imageUrlQueryNumber: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+    };
+
+    this.handleClickNoInterest = this.handleClickNoInterest.bind(this);
+    this.directToRandomProduct = this.directToRandomProduct.bind(this);
   }
 
   async componentDidMount() {
+    // console.log('componentDidMount');
     const { index } = qs.parse(this.props.location.search, {
       ignoreQueryPrefix: true,
     });
@@ -28,7 +38,7 @@ class Product extends React.Component {
     const jsonPlaceholderPost = await responsePlaceholderPost.json();
     this.setState({ dummyDescription: jsonPlaceholderPost?.body });
 
-    const recentList = Storage.get('recentList') ? Storage.get('recentList') : [];
+    const recentList = Storage.get(RECENT_LIST_STORAGE_KEY) ? Storage.get(RECENT_LIST_STORAGE_KEY) : [];
 
     // 이미 본 상품 또 클릭했을 경우
     if (recentList.indexOf(Number(index)) > -1) {
@@ -39,7 +49,55 @@ class Product extends React.Component {
       recentList.unshift(clickedProduct);
     } else recentList.unshift(Number(index));
 
-    Storage.set('recentList', recentList);
+    Storage.set(RECENT_LIST_STORAGE_KEY, recentList);
+  }
+
+  async componentDidUpdate(prevProps) {
+    // console.log('componentDidUpdate');
+    const { index: prevIndex } = qs.parse(prevProps.location.search, {
+      ignoreQueryPrefix: true,
+    });
+    const { index } = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    if (prevIndex !== index) {
+      // 상품 설명용 placeholder 데이터 업데이트
+      const responsePlaceholderPost = await fetch(`${PLACEHOLDER_POST_API}${Number(index) + 1}`);
+      const jsonPlaceholderPost = await responsePlaceholderPost.json();
+
+      this.setState({ dummyDescription: jsonPlaceholderPost?.body });
+    }
+    // console.log(`prev index : ${index}`);
+  }
+
+  // 관심 없음 클릭용
+  handleClickNoInterest(index) {
+    const { history } = this.props;
+    const noInterestList = Storage.get(NO_INTEREST_STORAGE_KEY) ? Storage.get(NO_INTEREST_STORAGE_KEY) : [];
+
+    noInterestList.unshift(Number(index));
+    Storage.set(NO_INTEREST_STORAGE_KEY, noInterestList);
+
+    this.directToRandomProduct(index);
+  }
+
+  // 랜덤 상품 조회용
+  directToRandomProduct(index) {
+    const { history } = this.props;
+    const noInterestList = Storage.get(NO_INTEREST_STORAGE_KEY) ? Storage.get(NO_INTEREST_STORAGE_KEY) : [];
+
+    // 다시 현재 상품으로 라우팅되지 않게 하기 위해 현 상품 index도 추가
+    noInterestList.unshift(Number(index));
+
+    // 관심 없는 상품 제외하고 랜덤 라우팅
+    const productsExceptNoInterest = [...Array(100).keys()].filter((elem) => noInterestList.indexOf(elem) === -1);
+    const randomIndex = productsExceptNoInterest[Math.floor(Math.random() * productsExceptNoInterest.length)];
+
+    // 다른 상품 페이지로 라우팅되면 이미지를 바꾸어주기 위함
+    this.setState({ imageUrlQueryNumber: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) });
+
+    history.replace(`/product?index=${productsExceptNoInterest[randomIndex]}`);
   }
 
   render() {
@@ -47,14 +105,14 @@ class Product extends React.Component {
       ignoreQueryPrefix: true,
     });
 
-    const { products, dummyDescription } = this.state;
+    const { products, dummyDescription, imageUrlQueryNumber } = this.state;
 
     // console.log(this.props.location);
     return (
       <ProductPageWrapper>
         <DetailsWrapper>
           <BigImg>
-            <img src="https://picsum.photos/360/480" alt="placeholder_image" />
+            <img src={`https://picsum.photos/360/480?dummy=${imageUrlQueryNumber}`} alt="product_random_image" />
           </BigImg>
 
           <DetailsContents>
@@ -67,8 +125,8 @@ class Product extends React.Component {
             </ContentTop>
 
             <ContentBottom>
-              <Button>관심 없음</Button>
-              <Button>랜덤 상품 조회</Button>
+              <Button onClick={() => this.handleClickNoInterest(index)}>관심 없음</Button>
+              <Button onClick={() => this.directToRandomProduct()}>랜덤 상품 조회</Button>
             </ContentBottom>
           </DetailsContents>
         </DetailsWrapper>
