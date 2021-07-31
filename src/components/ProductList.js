d349e328a1e2f024b4a7f9cc9835fa6f0aa6a51c
Jiyoon Hur <60052127+jiyoon1156@users.noreply.github.com>
2021-07-31 17:14:37 +0900

    [FEAT] ProductList 관심없음 상품 링크 접근 못하게 추가 (#32)

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,17 +2,45 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Storage from 'utils/Storage';
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleClick() {
+    // eslint-disable-next-line no-alert
+    alert('관심없는 상품입니다!!');
+  }
+
   render() {
     const { data } = this.props;
+    const accessDeniedItem = Storage.get('noInterest');
+
     return (
       <StyledShop>
-        {data.map((item, index) => (
-          <StyledProducts key={item.title} to={`/product?index=${index}`}>
-            <StyledTitle>{item.title}</StyledTitle>
-          </StyledProducts>
-        ))}
+        {data.map((item, index) =>
+          accessDeniedItem.includes(index) ? (
+            <StyledProductsNonLink key={item.title} onClick={this.handleClick}>
+              <StyledTitle>
+                {item.title}
+                <br />
+                {item.price}원
+              </StyledTitle>
+            </StyledProductsNonLink>
+          ) : (
+            <StyledProducts key={item.title} to={`/product?index=${index}`}>
+              <StyledTitle>
+                {item.title}
+                <br />
+                {item.price}원
+              </StyledTitle>
+            </StyledProducts>
+          ),
+        )}
       </StyledShop>
     );
   }
@@ -21,6 +49,18 @@ class ProductList extends React.Component {
 const StyledShop = styled.ul`
   display: flex;
   flex-wrap: wrap;
+`;
+
+const StyledProductsNonLink = styled.div`
+  width: 20%;
+  height: 15rem;
+  background: ${({ theme }) => theme.color.secondary};
+  box-shadow: 0 0 0 1px ${({ theme }) => theme.color.light} inset;
+  &:hover {
+    cursor: pointer;
+    background: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.light};
+  }
 `;
 
 const StyledProducts = styled(Link)`
